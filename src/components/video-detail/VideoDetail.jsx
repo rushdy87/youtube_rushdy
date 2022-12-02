8bbbe641278608fb41f8008d7fe2433634d88c44
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-12-02 11:11:54 +0300

    Video Details, end of the app'

--- src/components/video-detail/VideoDetail.jsx
+++ src/components/video-detail/VideoDetail.jsx
@@ -1,5 +1,87 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Box, Stack, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Videos } from '../../containers';
+import { fetchFromApi } from '../../utils/fetch-from-api';
+
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+      .then(({ data }) => setVideoDetail(data.items[0]))
+      .then((result) => {
+        return fetchFromApi(
+          `search?part=snippet&relatedToVideoId=${id}&type=video`
+        );
+      })
+      .then(({ data }) => setVideos(data.items))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  if (!videoDetail?.snippet) return <h1>Looding..</h1>;
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '95%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography variant="h5" color="#fff" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: '#fff' }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant="h6" color="#fff">
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
--- src/containers/videos/Videos.jsx
+++ src/containers/videos/Videos.jsx
@@ -2,15 +2,23 @@ import { Stack, Box } from '@mui/material';
 
 import { VideoCard, ChannelCard } from '../../components';
 
-const Videos = ({ videos }) => (
-  <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-    {videos.map((item, index) => (
-      <Box key={index}>
-        {item.id.videoId && <VideoCard video={item} />}
-        {item.id.channelId && <ChannelCard channelDetail={item} />}
-      </Box>
-    ))}
-  </Stack>
-);
+const Videos = ({ videos, direction }) => {
+  if (!videos) return 'Looding';
+  return (
+    <Stack
+      direction={direction || 'row'}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
 export default Videos;
